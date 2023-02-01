7a4098a78486972fbdb9847a9d8a4b176508795b
OGÜN ERKUTAY <77350593+ogunerkutay@users.noreply.github.com>
2023-02-01 18:41:54 +0300

    Ternary If kullanımına örnek uygulama.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TERNARYIFEHLIYET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yas = 0;
+            bool birsayimi = false;
+            
+            Console.WriteLine("Yaşınızı belirtin lütfen");
+            do
+            {
+                Console.WriteLine("lÜTFEN yaşınızı rakam olarak girin");
+                birsayimi = int.TryParse(Console.ReadLine(), out yas);
+            } while (birsayimi==false);
+            
+            string ehliyet = yas >= 18 ? "ALIR" : "ALAMAZ";
+
+            Console.WriteLine("ŞAHIS EHLİYET " + ehliyet);
+        }
+    }
+}
